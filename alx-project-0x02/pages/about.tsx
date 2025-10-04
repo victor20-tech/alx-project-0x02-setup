fd03649b042a5998187b4a3599f5299c1a7eefba
victor20-tech <victoradomako10@gmail.com>
2025-10-04 01:54:23 -0400

    fix error in Button.tsx and about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../components/common/Button';
+import Button from '@/components/common/Button';
 
 const About: React.FC = () => {
   const handleButtonClick = (name: string) => {
